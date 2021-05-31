1287211d10438ae407e996e8f010825d648bcc43
jdsalasca <jdsalasc@unal.edu.co>
2021-05-31 18:40:37 -0500

    .

--- src/Laucher/Runner.java
+++ src/Laucher/Runner.java
@@ -9,18 +9,11 @@ import src.ui.PrincipalMenu;
 public class Runner {
     static ReadingStory hiii = new ReadingStory("capitulo1.txt");
 
-
-
-
     public static void main(String[] args)  {
-  
-        
+         
         System.out.println("EL CABALLERO DE LA ARMADURA OXIDADA!");
         PrincipalMenu newUser = new PrincipalMenu();
-        newUser.principalMenu();
-
-
-            
+        newUser.principalMenu();    
         }
 
     }
